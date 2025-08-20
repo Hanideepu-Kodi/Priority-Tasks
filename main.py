8cd141e37715f39fc48d0ed6da7ba16992f2ee3c
PHOENIX-1040 <dhaarantenisha@gmail.com>
2025-08-20 22:21:58 +0530

    backend updated (completed date)

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-'''import uvicorn
-
-# Run 'python main.py' to start the backend
-if __name__ == "__main__":
-    uvicorn.run("backend.app:app", host="127.0.0.1", port=8000, reload=True)
-'''
-
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("backend.app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("backend.main:app", host="127.0.0.1", port=8000, reload=True)
